8554fd707ad5ece6388f8db9adcbba00291bce0d
Real-Chaos <rizwannafees13@gmail.com>
2023-10-21 16:12:22 -0400

    finished desining the add project form

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const toggleTheme = (function () {
   const root = document.documentElement
   root.className = 'light'
   checkboxToggler.addEventListener('change', () => {
-    
     checkboxToggler.checked
       ? (root.className = 'dark')
       : (root.className = 'light')
@@ -16,10 +15,29 @@ const toggleNav = (function () {
   navToggler.addEventListener('click', () => {
     console.log(sideNav.style.display)
     console.log(sideNav.style.transform)
-    sideNav.style.transform == 'translateX(-20vw)'
-      ? sideNav.style.transform = 'translateX(0vw)'
-      : sideNav.style.transform = 'translateX(-20vw)'
+    if (sideNav.style.transform == 'translateX(-20vw)') {
+      sideNav.style.transform = 'translateX(0vw)'
+      sideNav.style.width = '20vw'
+    } else {
+      sideNav.style.transform = 'translateX(-20vw)'
+      sideNav.style.width = '0vw'
+    }
 
-      sideNav.style.transform === 'translateX(0vw)' ? console.log('left -20vw') : console.log('left: 0vw')
+    sideNav.style.transform === 'translateX(0vw)'
+      ? console.log('left -20vw')
+      : console.log('left: 0vw')
+  })
+})()
+
+const toggleAddForm = (function () {
+  const addProjectText = document.querySelector('.add-project-text')
+  const addProjectForm = document.querySelector('.add-project-form')
+  const hideProjectForm = document.querySelector('.cancel')
+  addProjectText.addEventListener('click', () => {
+    addProjectForm.style.display = 'block'
+  })
+
+  hideProjectForm.addEventListener('click', () => {
+    addProjectForm.style.display = 'none'
   })
 })()
